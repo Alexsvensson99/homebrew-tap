3580ea23efb573cd38023ca91fac4f333b6a7e82
Alexander Svensson <alexandersvensson99@outlook.com>
2026-08-12 21:26:51 +0200

    Add PkgLift 0.1.1 formula

---
+++ Formula/pkglift.rb
@@ -0,0 +1,20 @@
+class Pkglift < Formula
+  desc "Safely migrate CocoaPods dependencies to Swift Package Manager"
+  homepage "https://github.com/Alexsvensson99/PkgLift"
+  url "https://github.com/Alexsvensson99/PkgLift/releases/download/v0.1.1/pkglift-macos-arm64.tar.gz"
+  sha256 "9b7e975abc60b6190fd765b96482521e503794a0da4cbf363be7996b366c5f1f"
+  license "MIT"
+
+  depends_on arch: :arm64
+  depends_on macos: :sonoma
+
+  def install
+    libexec.install "pkglift", "PkgLift_PkgLiftRegistry.bundle"
+    bin.install_symlink libexec/"pkglift"
+  end
+
+  test do
+    assert_equal "0.1.1", shell_output("#{bin}/pkglift version").strip
+    system bin/"pkglift", "registry", "validate"
+  end
+end
