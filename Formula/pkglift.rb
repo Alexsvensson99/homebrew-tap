c3763254f84a6c11deee604c4fb3b1df066a658d
Alexander Svensson <alexandersvensson99@outlook.com>
2026-08-22 21:37:38 +0200

    Update PkgLift to v0.4.0

--- Formula/pkglift.rb
+++ Formula/pkglift.rb
@@ -1,9 +1,9 @@
 class Pkglift < Formula
   desc "Safely migrate CocoaPods dependencies to Swift Package Manager"
   homepage "https://github.com/Alexsvensson99/PkgLift"
-  url "https://github.com/Alexsvensson99/PkgLift/releases/download/v0.3.0/pkglift-macos-arm64.tar.gz"
-  version "0.3.0"
-  sha256 "8ce86b9fbc591797258d97aeb509994c0e092f1a1b351ba1e0cf76225549cf71"
+  url "https://github.com/Alexsvensson99/PkgLift/releases/download/v0.4.0/pkglift-macos-arm64.tar.gz"
+  version "0.4.0"
+  sha256 "52456f05d385dadd227b3fb053366c46c95ef87947aa159b53a8740eb275301e"
   license "MIT"
 
   depends_on arch: :arm64
@@ -15,7 +15,7 @@ class Pkglift < Formula
   end
 
   test do
-    assert_equal "0.3.0", shell_output("#{bin}/pkglift version").strip
+    assert_equal "0.4.0", shell_output("#{bin}/pkglift version").strip
     system bin/"pkglift", "registry", "validate"
   end
 end
